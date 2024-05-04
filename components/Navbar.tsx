5e9453117944c2037254eb0354edbddad321fe2a
bosco0808 <bosco0808@gmail.com>
2024-05-04 13:40:37 +0800

    minor change :)

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link href={"/"} className="navbar-brand">
+      <Link href={"/"} className="navbar-brand m-2">
         NTHYKYLDSS
       </Link>
-      <Link href={"/"} className="nav-item nav-link">
+      <Link href={"/"} className="nav-item nav-link m-2">
         Home
       </Link>
     </nav>
